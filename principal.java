17f9ed9bbe3cc7bf181ac0d74ac29b43f8874ad2
alisandro <37528290+alisandro@users.noreply.github.com>
2020-12-04 08:59:49 -0600

    Update principal.java

--- principal.java
+++ principal.java
@@ -3,7 +3,7 @@ package log;
 public class principal {
 
 	public static void main(String[] args) {
-		LogPPA xd = new LogPPA("C:/spring/workspacelogs/LogPPA2/", "Bancyhosted.log.PPA", NivelesPPA.INFO);
+		LogPPA xd = new LogPPA("C:/spring/workspacelogs/LogPPA2/", "nombre de log", NivelesPPA.INFO);
 		
 		for (int i = 0; i <100000; i++) {
 //			xd.informacion("exito", "pancha", "1.1.1","dasdas", "", "evento"+i);
